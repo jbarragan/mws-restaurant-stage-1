4d730937351d0d2a8c68d2bd7aa236f05838ffcc
Jaime Barragan <jaime.barragan@gmail.com>
2018-06-12 08:22:12 -0600

    bug: Fix map role and use correct header level (h1, h2, h3)

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -60,7 +60,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
-  image.alt = restaurant.name;
+  image.alt = "Image of " + restaurant.name + " Restaurant";
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
@@ -99,7 +99,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
