977e8fb75d4236de267e21e6ca7bd836a4d32b65
Jaime Barragan <jaime.barragan@gmail.com>
2018-07-10 10:54:36 -0600

    feat: Use fetch to interact with API

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -15,39 +15,20 @@ class DBHelper {
   /**
    * Fetch all restaurants.
    */
+
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch( DBHelper.DATABASE_URL )
+    .then( response => response.json().then( json => callback(null,json) ) )
+    .catch( error => callback(error, null) );
   }
 
   /**
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-    // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
-          callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
-        }
-      }
-    });
+    fetch( `${DBHelper.DATABASE_URL}/${id}` )
+    .then( response => response.json().then( json => callback(null,json) ) ) //callback(null, response.json()) )
+    .catch( error => callback(`${error}: Restaurant does not exist`, null) );
   }
 
   /**
